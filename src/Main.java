c1883d1ecb65c92e037d7c85d10e7751948a600c
azeALI <alihesenzade5000@gmail.com>
2024-11-18 02:14:46 +0400

    quick fix

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
-
 import java.io.IOException;
 import java.util.Scanner;
-
+import java.io.File;
 public class Main {
-    public static void main(String[] args) throws IOException {
-        API.api = new Scanner("api.txt").nextLine();
-        new Login();
-    }
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(new File("api.txt"));
+    API.api = sc.nextLine();
+    sc.close();
+    System.out.println(API.api);
+    new Login();
+  }
 }
